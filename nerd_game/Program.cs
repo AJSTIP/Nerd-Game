067a8f06f19cd7f1a32f533445aacb1ca8205088
ajstip55 <ajstip55@outlook.com>
2021-09-23 10:32:44 -0400

    commit

--- nerd_game/Program.cs
+++ nerd_game/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace nerd_game
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Player.certifiedHoodClassic();
+            Console.ReadKey();
+        }
+       
+    }
+
+    public class Player
+    {
+        public static void certifiedHoodClassic()
+        {
+            Console.WriteLine("How many players do you have? 2-8");
+
+            int playerCount  = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What's the Player's name?");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("What's the Player's age?");
+            int playerAge = Convert.ToInt32(Console.ReadLine());
+            var rand = new Random();
+            double playerLuck = rand.Next(0, 10);
+            int playerRoll = rand.Next(1, 10);
+            
+
+
+
+            Console.WriteLine($"You have {playerCount} players.");
+            Console.WriteLine($"Player name is {playerName}.");
+            Console.WriteLine($"Player age is {playerAge}.");
+            Console.WriteLine($"Player luck is 1.{playerLuck}.");
+            Console.WriteLine($"Player roll is {playerRoll}.");
         }
     }
 }
